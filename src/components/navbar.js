ef83124ea53df6bc6289c5400f2df3e2202e909d
Rabten Samphel <rabten@protonmail.com>
2022-05-12 18:54:00 -0400

    created mobile menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,21 @@
 import { Link } from "react-router-dom";
 
 function Navbar() {
+    const showMobileMenu = () => {
+        const mobileMenu = document.querySelector('.mobileMenu');
+        const content = document.querySelector('.mobileMenuContent');
+
+        if (mobileMenu.style.height === '100vh') {
+            mobileMenu.style.height = '0';
+            content.style.display = 'none';
+        } else {
+            mobileMenu.style.height = '100vh';
+            setTimeout(() => {
+                content.style.display = 'flex';
+            }, 100);
+        };
+    };
+
     return(
         <div className='navbar'>
             <div className='navOne navBox'>
@@ -41,10 +56,46 @@ function Navbar() {
                 </li>
             </div>
 
-            <div className='hamburger'>
+            <div className='hamburger' onClick={showMobileMenu}>
                 <span></span>
                 <span></span>
                 <span></span>
+            </div>
+
+            <div className="mobileMenu">
+                <div className="mobileMenuContent">
+                    <Link to='/'>
+                        <li>
+                            Home
+                        </li>
+                    </Link>
+    
+                    <Link to='/jersey'>
+                        <li>
+                            Jerseys
+                        </li>
+                    </Link>
+
+                    <li>
+                        Shorts
+                    </li>
+
+                    <li>
+                        Footwears
+                    </li>
+
+                {/* <Link>
+                    <li>
+                        Shorts
+                    </li>
+                </Link>
+
+                <Link>
+                    <li>
+                        Footwears
+                    </li>
+                </Link> */}
+                </div>
             </div>
         </div>
     );
