162ded23bfaabbd01ac66fd01e4b0b80e731f9fa
Rabten Samphel <rabten@protonmail.com>
2022-05-14 19:39:44 -0400

    finished intro section

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import carousalData from "../assets/carousalData";
+import jersey from "../assets/soccer-jersey.png";
+import shorts from "../assets/football-shorts.png";
+import cleats from "../assets/cleats.png";
+import ball from "../assets/football.png";
 
 function Home() {    
     const [cur, setcur] = useState(0);
@@ -53,16 +57,52 @@ function Home() {
                             best Football uniforms and cleats at the cheapest 
                             prices as possible. Don't doubt us though, our cheap
                             prices doesn't equate to a poor quality merchandise. 
-                            We sell authentic top quality kits straight from the 
+                            We sell authentic top quality items straight from 
                             manufacturer such as Nike, Adidas and Puma.
                         </p>
                     </div>
 
                     <div className="introTwoTwo">
-                        <div className="boxes"></div>
-                        <div className="boxes"></div>
-                        <div className="boxes"></div>
-                        <div className="boxes"></div>
+                        <div className="boxes">
+                            <img src={jersey} alt='jersey' />
+
+                            <div className="boxes2">
+                                <h1>
+                                    We got Jerseys
+                                </h1>
+                            </div>
+                        </div>
+
+                        <div className="boxes">
+                            <img src={shorts} alt='shorts' />
+
+                            <div className="boxes2">
+                                <h1>
+                                    We got Shorts
+                                </h1>
+                            </div>
+                        </div>
+
+                        <div className="boxes">
+                            <img src={cleats} alt='cleats' />
+
+                            <div className="boxes2">
+                                <h1>
+                                    We got Cleats
+                                </h1>
+                            </div>
+                        </div>
+
+
+                        <div className="boxes">
+                            <img src={ball} alt='ball' />
+
+                            <div className="boxes2">
+                                <h1>
+                                    We got everything
+                                </h1>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
